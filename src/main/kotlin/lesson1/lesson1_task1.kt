2d5c57cdae2e88f52694baf47993b5ad708adfac
Artem <belyshev1311@gmail.com>
2025-12-03 18:38:55 +0500

    Lesson1.1

---
+++ src/main/kotlin/lesson1/lesson1_task1.kt
@@ -0,0 +1,10 @@
+package org.example
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val ordersAmount = 75
+    val thanksText = "Thank you for your purchase!"
+    println(ordersAmount)
+    println(thanksText)
+}
